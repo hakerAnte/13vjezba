3cf2e88f4b06f0253c72af5a2f06193dda3a831b
hakerAnte <filipovicpatrick1@gmail.com>
2017-05-15 09:27:07 +0200

    ispisuje se glavni izbornik

---
+++ 13. vjezba-filipovic/zbroj.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "glavni izbornik" << endl;
+    cout << "1. unos nove osobe" << endl;
+    cout << "2. ispisi podatke" << endl;
+    cout << " 3, pretraga prema racunu" << endl;
+    cout << "4. pretraga prema prezimenu" << endl;
+    cout << "5. ispisi sortirano" << endl;
+    int izbor;
+    cin >> izbor;
+}
